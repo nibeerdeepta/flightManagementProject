48ddbbd571c59a6ad9f903a2b0cc2b7cb5e705ef
nibeerdeepta <nibeerdeepta@gmail.com>
2025-01-05 00:46:50 +0600

    creating child class

--- flight.cpp
+++ flight.cpp
@@ -1,12 +1,47 @@
 #include<iostream>
 using namespace std;
 
+class Flight{
+protected:
+string flightnumber;
+string departureTime;
+string destination;
+float price;
+
+public:
+//virtual void inputFlightDetails()=0;
+//virtual void displayFlightDetails()=0;
+//virtual ~Flight(){}
+
+};
+
+class DomesticFlight : public Flight {
+private:
+    string departureCity;
+    string arrivalCity;
+    
+public:
+    // Constructor for Domestic Flight
+    DomesticFlight(string flightNum, string depTime, double flightPrice, string depCity, string arrCity)
+        : Flight(flightNum, depTime, flightPrice) {
+        departureCity = depCity;
+        arrivalCity = arrCity;
+    }
+
+    // Override the displayFlightDetails function
+    void displayFlightDetails() override {
+        cout << "\n** Domestic Flight **\n";
+        Flight::displayFlightDetails(); // Display base class details
+        cout << "Departure City: " << departureCity << endl;
+        cout << "Arrival City: " << arrivalCity << endl;
+    }
+};
 
 int main()
 {
 
+Flight f1;
 cout<<"Hello World";
-
 return 0;
 
 }
